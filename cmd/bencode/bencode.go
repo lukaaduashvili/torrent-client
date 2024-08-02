901e993890266583bb07f7fb663303e932ccd764
lukaaduashvili <lukaaduashvili@gmail.com>
2024-08-02 20:30:10 +0400

    Parse Lists

---
+++ cmd/bencode/bencode.go
@@ -0,0 +1,81 @@
+package bencode
+
+import (
+	"fmt"
+	"strconv"
+	"unicode"
+)
+
+func decodeString(bencodedString string) (res string, length int, err error) {
+	var firstColonIndex int
+
+	for i := 0; i < len(bencodedString); i++ {
+		if bencodedString[i] == ':' {
+			firstColonIndex = i
+			break
+		}
+	}
+
+	lengthStr := bencodedString[:firstColonIndex]
+
+	length, err = strconv.Atoi(lengthStr)
+	if err != nil {
+		return "", 0, err
+	}
+
+	return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], length + firstColonIndex + 1, nil
+}
+
+func decodeInt(bencodedString string) (res int, length int, err error) {
+	var startIdx int
+	var endIdx int
+	for i := 0; i < len(bencodedString); i++ {
+		if bencodedString[i] == 'i' {
+			startIdx = i + 1
+		}
+		if bencodedString[i] == 'e' {
+			endIdx = i
+			break
+		}
+	}
+
+	res, err = strconv.Atoi(bencodedString[startIdx:endIdx])
+
+	return res, endIdx + 1, err
+}
+
+func decodeList(bencodedString string) (res []interface{}, length int, err error) {
+	result := make([]interface{}, 0)
+
+	for i := 1; i < len(bencodedString); {
+		val, length, err := Decode(bencodedString[i:])
+		if err != nil {
+			return nil, 0, err
+		}
+
+		result = append(result, val)
+
+		i += length
+
+		if i >= len(bencodedString) {
+			return nil, 0, fmt.Errorf("List decoding out of bounds")
+		}
+		if bencodedString[i] == 'e' {
+			return result, i, nil
+		}
+	}
+
+	return nil, 0, fmt.Errorf("List missing ending character")
+}
+
+func Decode(bencodedString string) (interface{}, int, error) {
+	if unicode.IsDigit(rune(bencodedString[0])) {
+		return decodeString(bencodedString)
+	} else if bencodedString[0] == 'i' {
+		return decodeInt(bencodedString)
+	} else if bencodedString[0] == 'l' {
+		return decodeList(bencodedString)
+	} else {
+		return "", 0, fmt.Errorf("only strings are supported at the moment")
+	}
+}
--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -2,55 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	// Uncomment this line to pass the first stage
-	// "encoding/json"
 	"fmt"
+	"github.com/codecrafters-io/bittorrent-starter-go/cmd/bencode"
 	"os"
-	"strconv"
-	"unicode"
 	// bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
-
-// Example:
-// - 5:hello -> hello
-// - 10:hello12345 -> hello12345
-func decodeBencode(bencodedString string) (interface{}, error) {
-	if unicode.IsDigit(rune(bencodedString[0])) {
-		var firstColonIndex int
-
-		for i := 0; i < len(bencodedString); i++ {
-			if bencodedString[i] == ':' {
-				firstColonIndex = i
-				break
-			}
-		}
-
-		lengthStr := bencodedString[:firstColonIndex]
-
-		length, err := strconv.Atoi(lengthStr)
-		if err != nil {
-			return "", err
-		}
-
-		return bencodedString[firstColonIndex+1 : firstColonIndex+1+length], nil
-	} else if bencodedString[0] == 'i' {
-		var startIdx int
-		var endIdx int
-		for i := 0; i < len(bencodedString); i++ {
-			if bencodedString[i] == 'i' {
-				startIdx = i + 1
-			}
-			if bencodedString[i] == 'e' {
-				endIdx = i - 1
-				break
-			}
-		}
-
-		return strconv.Atoi(bencodedString[startIdx : endIdx+1])
-	} else {
-		return "", fmt.Errorf("Only strings are supported at the moment")
-	}
-}
 
 func main() {
 	command := os.Args[1]
@@ -58,7 +14,7 @@ func main() {
 	if command == "decode" {
 		bencodedValue := os.Args[2]
 
-		decoded, err := decodeBencode(bencodedValue)
+		decoded, _, err := bencode.Decode(bencodedValue)
 		if err != nil {
 			fmt.Println(err)
 			return
